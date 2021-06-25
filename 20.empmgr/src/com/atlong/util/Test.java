3be73ab91630da0cfda7f6d72c2738df8f23c0ad
Datalong <2922722656@qq.com>
2021-06-25 22:02:58 +0800

    mybatis第一次

---
+++ 20.empmgr/src/com/atlong/util/Test.java
@@ -0,0 +1,13 @@
+package com.atlong.util;
+
+/**
+ * @Auther: Datalong
+ * @Date: 2021/6/25 - 06 -25 -22:00
+ * @Dscription: com.atlong.util
+ * @version: 1.0
+ */
+public class Test {
+    public static void main(String[] args) {
+
+    }
+}
